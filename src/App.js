d35fce0deebee221f200467648299ba7d4ac51ea
Roch Armengaud <armengaud.roch@live.fr>
2023-01-30 18:59:03 +0100

    Shortened code for app.js

--- src/App.js
+++ src/App.js
@@ -9,32 +9,13 @@ const Error404 = lazy(() => import('./pages/404'));
 const App = () => {
   return (
     <div className="App">
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <Suspense fallback={<h1>Loading...</h1>}>
-              <Homepage />
-            </Suspense>
-          }
-        />
-        <Route
-          path="/employee-list"
-          element={
-            <Suspense fallback={<h1>Loading...</h1>}>
-              <EmployeeList />
-            </Suspense>
-          }
-        />
-        <Route
-          path="/*"
-          element={
-            <Suspense fallback={<h1>Loading...</h1>}>
-              <Error404 />
-            </Suspense>
-          }
-        />
-      </Routes>
+      <Suspense fallback={<h1>Loading...</h1>}>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/employee-list" element={<EmployeeList />} />
+          <Route path="/*" element={<Error404 />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 };
